41199b19e7a1d1bb8f093b8f33fa45e8dd63d42c
YahirMendez <yahirm_24@alu.uabcs.mx>
2026-02-20 10:51:11 -0700

    prediseño login

--- src/programacion3/prueba1.java
+++ src/programacion3/prueba1.java
@@ -8,17 +8,7 @@ public class prueba1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JFrame ventana = new JFrame();
-		ventana.setVisible(true);
-		ventana.setSize(500,500);
-		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		ventana.setLocationRelativeTo(null);
-		ventana.setMinimumSize(new Dimension(200,200));
-		ventana.setMaximumSize(new Dimension(800,800));
-		ventana.setTitle("hola");
-		//ventana.setLocation(0, 0);
-		
-
+		Ventana ventana = new Ventana();
 	}
 
 }
