12f1651aeb6559073f8c42b6ee455490a530281d
YahirMendez <yahirm_24@alu.uabcs.mx>
2026-02-22 17:07:31 -0700

    unidad 2 ejercicio 10 o diseño de login

--- src/programacion3/Ventana.java
+++ src/programacion3/Ventana.java
@@ -5,9 +5,11 @@ import java.awt.Dimension;
 import java.awt.Font;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class Ventana extends JFrame {
@@ -27,8 +29,7 @@ public class Ventana extends JFrame {
 		
 		JPanel contenedor = new JPanel();
 		contenedor.setOpaque(true);
-		contenedor.setBackground(Color.black);
-		contenedor.setBackground(Color.black);
+		contenedor.setBackground(Color.white);
 		contenedor.setSize(500,500);
 		contenedor.setLocation(0, 0);
 		contenedor.setLayout(null);
@@ -36,27 +37,77 @@ public class Ventana extends JFrame {
 	
 		JLabel title_login = new JLabel();
 		title_login.setText("Bienvenido");
-		title_login.setSize(200, 30);
+		title_login.setSize(200, 50);
 		title_login.setOpaque(true);
-		title_login.setLocation(145, 10);
+		title_login.setLocation(145, 20);
 		title_login.setBackground(Color.white);
 		title_login.setFont(new Font("Arial",Font.BOLD,22));
+		title_login.setForeground(Color.blue);
 		title_login.setHorizontalAlignment(JLabel.CENTER);
 		contenedor.add(title_login);
 		
+		JLabel labelUsername = new JLabel();
+		labelUsername.setText("Usuario: ");
+		labelUsername.setSize(220, 50);
+		labelUsername.setOpaque(true);
+		labelUsername.setLocation(30, 80);
+		labelUsername.setBackground(Color.white);
+		labelUsername.setFont(new Font("Arial",Font.BOLD,22));
+		labelUsername.setForeground(Color.blue);
+		labelUsername.setHorizontalAlignment(JLabel.CENTER);
+		contenedor.add(labelUsername);
+		
 		JTextField username = new JTextField();
-		username.setSize(300, 30);
-		username.setLocation(95,100);
+		username.setSize(300, 50);
+		username.setLocation(85,120);
 		username.setFont(new Font("Arial",Font.BOLD,22));
+		username.setForeground(Color.black);
+		username.setHorizontalAlignment(JLabel.CENTER);
 		contenedor.add(username);
+		
+		JLabel labelPassword = new JLabel();
+		labelPassword.setText("Contraseña: ");
+		labelPassword.setSize(200, 50);
+		labelPassword.setOpaque(true);
+		labelPassword.setLocation(60, 180);
+		labelPassword.setBackground(Color.white);
+		labelPassword.setFont(new Font("Arial",Font.BOLD,22));
+		labelPassword.setForeground(Color.blue);
+		labelPassword.setHorizontalAlignment(JLabel.CENTER);
+		contenedor.add(labelPassword);
+		
+		JPasswordField password = new JPasswordField();
+		password.setSize(300, 50);
+		password.setLocation(85,220);
+		password.setFont(new Font("Arial",Font.BOLD,22));
+		password.setForeground(Color.black);
+		password.setHorizontalAlignment(JLabel.CENTER);
+		contenedor.add(password);
 	
 		JButton acceder = new JButton();
 		acceder.setText("Acceder");
-		acceder.setLocation(140, 400);
+		acceder.setLocation(140, 380);
 		acceder.setSize(200, 50);
 		acceder.setFont(new Font("Arial",Font.BOLD,22));
+		acceder.setBackground(Color.blue);
+		acceder.setForeground(Color.white);
 		contenedor.add(acceder);
 		
+		JCheckBox checkbox = new JCheckBox();
+		checkbox.setText("Recordarme");
+		checkbox.setBounds(85, 290, 100, 20);
+		contenedor.add(checkbox);
+		
+		JLabel labelRPassword = new JLabel();
+		labelRPassword.setText("¿Olvido su contraseña?");
+		labelRPassword.setSize(200, 20);
+		labelRPassword.setOpaque(true);
+		labelRPassword.setLocation(230, 290);
+		labelRPassword.setBackground(Color.white);
+		labelRPassword.setFont(new Font("Arial",Font.BOLD,12));
+		labelRPassword.setForeground(Color.blue);
+		labelRPassword.setHorizontalAlignment(JLabel.CENTER);
+		contenedor.add(labelRPassword);
 		
 		contenedor.repaint();
 		contenedor.revalidate();
